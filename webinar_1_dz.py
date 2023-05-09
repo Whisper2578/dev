f16ae7e4163a60d5a3d619fced2f6c9b885d9143
Antonov Andrey <114511726+Whisper2578@users.noreply.github.com>
2023-05-09 20:06:08 +0300

    Add files via upload

---
+++ webinar_1_dz.py
@@ -0,0 +1,8 @@
+name = 'Андрей'
+surname = 'Антонов'
+experience = '5 месяцев'
+position = 'Инженер-тестировщик (3 категории)'
+
+print('Фамилия Имя:', surname, name)
+print('Стаж работы в Тензоре:', experience)
+print('Должность:', position)
